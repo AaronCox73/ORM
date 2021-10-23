115b3a0b4795c9044f9a764496dce0c449b9a875
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-10-23 12:30:40 -0600

    first issue

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection')
+const mysql = require('mysql2')
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
